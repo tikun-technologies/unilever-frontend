9834a2de4782af52fcf22124d2b4407d97d1324f
Tushar Pareenja <tusharpareenja@gmail.com>
2026-04-09 19:46:21 +0530

    Update projectApi.ts

--- api/projectApi.ts
+++ api/projectApi.ts
@@ -134,7 +134,6 @@ export async function startProjectZipExport(projectId: string): Promise<{ job_id
     }
     return res.json();
 }
-
 /**
  * Update a project
  * PUT /api/v1/projects/{project_id}
